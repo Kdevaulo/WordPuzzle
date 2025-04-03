b8ca48eb582d4249dfca6e6e90e542ecc560a381
Kdevaulo <kdevaulo@gmail.com>
2025-04-03 17:28:54 +0500

    Utilities added
    Target framerate setter
    SafeArea component

---
+++ Assets/Kdevaulo/Scripts/Utilities/FrameRateLimiter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Kdevaulo.WordPuzzle.Utilities
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu(nameof(FrameRateLimiter) + " in " + nameof(Utilities))]
+    public class FrameRateLimiter : MonoBehaviour
+    {
+        private void Awake()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+        }
+    }
+}
